4dacc2ddea227f39fe19e52d4baa23a90f6f01d1
ahmeddaruwala <ahmeddaruwala360@gmail.com>
2024-09-13 22:47:26 +0500

    console the set of arrays with different number of values

---
+++ array/adding-array.js
@@ -0,0 +1,21 @@
+const arrayOfArrays = [
+    [1, 5, 7],
+    [4, 9],
+    [6, 4, 3, 8, 2]
+];
+
+for (let i = 0; i < arrayOfArrays.length; i++) {
+    for (let j = 0; j < arrayOfArrays[i].length; j++) {
+        // console.log(arrayOfArrays[i][j]);
+    }
+}
+console.log(arrayOfArrays[0][0]);
+console.log(arrayOfArrays[0][1]);
+console.log(arrayOfArrays[0][2]);
+console.log(arrayOfArrays[1][0]);
+console.log(arrayOfArrays[1][1]);
+console.log(arrayOfArrays[2][0]);
+console.log(arrayOfArrays[2][1]);
+console.log(arrayOfArrays[2][2]);
+console.log(arrayOfArrays[2][3]);
+console.log(arrayOfArrays[2][4]);
